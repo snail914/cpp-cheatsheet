27c889959dcbe4fc31e1e5d06510211e2f2a27c9
Kai LI <kai_li@outlook.com>
2018-07-18 12:00:25 +0100

    Update kli_gmock.h

--- kli_gmock.h
+++ kli_gmock.h
@@ -4,47 +4,43 @@
 https://github.com/google/googletest/blob/master/googlemock/docs/CheatSheet.md
  * */
 
-class Inner
+Struct Msg
 {
+    void print() const {}
+    
+    const std::string& get() const {}
 
+    int append(const int) {}                // overloads 
+    
+    int append(const std::string&) {}       // overloads 
 };
 
-template <typename T=Inner>
-class Outer
+class MockMsg 
 {
-    const T t;
+    MOCK_CONST_METHOD0(print, void(void));
+    
+    MOCK_CONST_METHOD0(get, const std::string&(void));
+   
+    MOCK_METHOD1(append, int(const int));
+    MOCK_METHOD1(append, int(const std::string&));
+}
 
-    Outer(const T& tt) // only works with reference
-    : t{tt}
+template <typename MSG=Msg>
+class Job
+{
+    const MSG d_msg;
+
+    Job(const MSG& msg) // only works with reference
+    : d_msg{msg}
     {}
 
-    int doSomething() const
-    {
-        int ret = 0;
-        std::string s;
-        ret = t.doS(&s);
-        return ret;
-    }
-
-    bool copyValue(int* to, int from)
-    {
-        return t.setV(to, from);
-    }
-};
-
-class MockInner
-{
-    MOCK_CONST_METHOD1(doSomething, int(void));
-    MOCK_METHOD2(copyValue, bool(int*, int));
 };
 
 
 TEST(Test, WithMock)
 {
-    MockInner inner;
-    Outer<MockInner> outer{inner};
-    std::string s{"56"};
-    int age = 90;
+    MockMsg msg;
+    Job<MockMsg> job{msg};
 
     EXPECT_CALL(inner, doS(_))
     .WillOnce(DoAll(SetArgPointee<0>(s), Return(age)));
